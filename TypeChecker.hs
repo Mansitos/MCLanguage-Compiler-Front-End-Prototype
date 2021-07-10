7c81705c8f9da455a02be687bbb5eb7c3c4aafc6
ChristianCagnoni <73893464+ChristianCagnoni@users.noreply.github.com>
2021-07-10 18:13:36 +0200

    update env + add type checker elements

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -1,7 +1,8 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module TypeChecker where
 
 import Type
-import LexProgettoPar (Posn)
+import LexProgettoPar (Posn(..))
 import AbsProgettoPar as Abs
 import Data.Map
 import Prelude
@@ -23,6 +24,21 @@ data TCheckResult
     | TError {errors::[Prelude.String]}
     deriving (Show)
 
+first :: (Env,[Prelude.String]) -> Env
+first (f,s) = f
+
+second :: (Env,[Prelude.String]) -> [Prelude.String]
+second (f,s) = s
+
+updateEnv :: (Abs.STATEMENTS Posn) -> Env -> [Prelude.String] -> (Env,[Prelude.String])
+updateEnv node@(Abs.ListStatements pos stat stats) env err = case stat of
+                                                                Abs.ReturnStatement pos ret-> (insertWith (++) "return" [Variable (B_type Type_Void) (Pn 0 1 1) False] env,err ++ checkErr env stat)
+                                                                _ -> (env,err)
+updateEnv node@(Abs.EmptyStatement  pos) env err = (env,err)
+
+checkErr :: Env -> Abs.STATEMENT Posn -> [Prelude.String]
+checkErr env stat = []                    
+
 --------------------------------------
 -- MAIN FUNCTIONS FOR TYPE CHECKING --
 --------------------------------------
@@ -37,23 +53,106 @@ data TCheckResult
 executeTypeChecking :: Abs.S Posn -> Env -> Abs.S TCheckResult
 executeTypeChecking node@(Abs.StartCode _ statements) start_env = Abs.StartCode (checkTypeStatements node start_env) (executeStatements statements start_env)
 
-checkTypeStatements:: Abs.S Posn -> Env -> TCheckResult
-checkTypeStatements (Abs.StartCode _ statements) start_env = TError ["Coming soon :)"]
-
-checkTypeStatement :: Abs.STATEMENT Posn -> Env -> TCheckResult
-checkTypeStatement node@(Abs.BreakStatement pos) env = checkTypeBreakStatement node env
-
 executeStatements :: Abs.STATEMENTS Posn -> Env -> Abs.STATEMENTS TCheckResult                      -- stat or node?
-executeStatements node@(Abs.ListStatements _ stat stats) env = Abs.ListStatements (checkTypeStatement stat env) (executeStatement stat env) (executeStatements stats env)
+executeStatements node@(Abs.ListStatements _ stat stats) env = let newEnv = updateEnv node env [] in 
+                                                                Abs.ListStatements (checkTypeStatement stat env) (executeStatement stat env) (executeStatements stats (first newEnv))
 executeStatements node@(Abs.EmptyStatement _) env = Abs.EmptyStatement (checkEmptyStatement node env)
 
 executeStatement :: Abs.STATEMENT Posn -> Env -> Abs.STATEMENT TCheckResult
 executeStatement node@(Abs.BreakStatement _) env = Abs.BreakStatement (checkTypeBreakStatement node env)
+executeStatement node@(Abs.ContinueStatement _) env = Abs.ContinueStatement (checkTypeContinueStatement node env)
+executeStatement node@(Abs.ReturnStatement _ ret) env = Abs.ReturnStatement (checkTypeReturnStatement node env) (executeReturnStatement ret env)
+executeStatement node@(Abs.Statement _ b) env = Abs.Statement (checkTypeStatement node env) (executeB b env)
+
+executeB :: Abs.B Posn -> Env -> Abs.B TCheckResult
+executeB node@(Abs.BlockStatement pos statements) env = Abs.BlockStatement (checkTypeB node env) (executeStatements statements env)
+
+executeReturnStatement :: Abs.RETURNSTATEMENT Posn -> Env -> Abs.RETURNSTATEMENT TCheckResult
+executeReturnStatement node@(Abs.ReturnState _ retExp) env = Abs.ReturnState (checkTypeReturnState node env) (executeExpression retExp env)
+
+executeExpression :: Abs.EXPRESSION Posn -> Env -> Abs.EXPRESSION TCheckResult
+executeExpression node@(Abs.ExpressionInteger pos value) env = Abs.ExpressionInteger (checkTypeExpresion node env) (executeInteger value env)
+executeExpression node@(Abs.ExpressionBoolean pos value) env = Abs.ExpressionBoolean (checkTypeExpresion node env) (executeBoolean value env)
+executeExpression node@(Abs.ExpressionChar pos value) env = Abs.ExpressionChar (checkTypeExpresion node env) (executeChar value env)
+executeExpression node@(Abs.ExpressionString pos value) env = Abs.ExpressionString (checkTypeExpresion node env) (executeString value env)
+executeExpression node@(Abs.ExpressionReal pos value) env = Abs.ExpressionReal (checkTypeExpresion node env) (executeReal value env)
+
+executeInteger :: Abs.Integer Posn -> Env -> Abs.Integer TCheckResult
+executeInteger node@(Abs.Integer value pos) env = Abs.Integer value (TResult env (B_type Type_Integer ) pos)
+
+executeChar :: Abs.Char Posn -> Env -> Abs.Char TCheckResult
+executeChar node@(Abs.Char value pos) env = Abs.Char value (TResult env (B_type Type_Char ) pos)
+
+executeString :: Abs.String Posn -> Env -> Abs.String TCheckResult
+executeString node@(Abs.String value pos) env = Abs.String value (TResult env (B_type Type_String ) pos)
+
+executeReal :: Abs.Real Posn -> Env -> Abs.Real TCheckResult
+executeReal node@(Abs.Real value pos) env = Abs.Real value (TResult env (B_type Type_Real ) pos)
+
+executeBoolean :: Abs.Boolean Posn -> Env -> Abs.Boolean TCheckResult
+executeBoolean node@(Abs.Boolean_true pos) env = Abs.Boolean_true (TResult env (B_type Type_Boolean ) pos)
+executeBoolean node@(Abs.Boolean_True pos) env = Abs.Boolean_True (TResult env (B_type Type_Boolean ) pos)
+executeBoolean node@(Abs.Boolean_false pos) env = Abs.Boolean_false (TResult env (B_type Type_Boolean ) pos)
+executeBoolean node@(Abs.Boolean_False pos) env = Abs.Boolean_False (TResult env (B_type Type_Boolean ) pos)
+
+checkTypeStatements:: Abs.S Posn -> Env -> TCheckResult
+checkTypeStatements (Abs.StartCode _ statements) start_env = case statements of
+                                                                (Abs.EmptyStatement pos) -> checkEmptyStatement (Abs.EmptyStatement pos) start_env
+                                                                (Abs.ListStatements pos stat stats) -> checkTypeStatement stat start_env
 
 checkEmptyStatement :: Abs.STATEMENTS Posn -> Env -> TCheckResult
 checkEmptyStatement (Abs.EmptyStatement pos) env = TResult env (B_type Type_Void) pos
+
+checkTypeStatement :: Abs.STATEMENT Posn -> Env -> TCheckResult
+checkTypeStatement node@(Abs.BreakStatement pos) env = checkTypeBreakStatement node env
+checkTypeStatement node@(Abs.ContinueStatement pos) env = checkTypeContinueStatement node env
+checkTypeStatement node@(Abs.ReturnStatement pos ret) env = checkTypeReturnStatement node env
+checkTypeStatement node@(Abs.Statement pos b) env = checkTypeB b env
+
+checkTypeB :: Abs.B Posn -> Env -> TCheckResult
+checkTypeB node@(Abs.BlockStatement pos statements) env = case statements of
+                                                    (Abs.EmptyStatement pos) -> checkEmptyStatement (Abs.EmptyStatement pos) env
+                                                    (Abs.ListStatements pos stat stats) -> checkTypeStatement stat env
 
 checkTypeBreakStatement :: Abs.STATEMENT Posn -> Env -> TCheckResult
 checkTypeBreakStatement (Abs.BreakStatement pos) env = case Data.Map.lookup "while" env of
     Just result -> TResult env (B_type Type_Void) pos
     Nothing -> TError ["Unexpected break statement at " ++ (show pos)]
+
+checkTypeContinueStatement :: Abs.STATEMENT Posn -> Env -> TCheckResult
+checkTypeContinueStatement (Abs.ContinueStatement pos) env = case Data.Map.lookup "while" env of
+    Just result -> TResult env (B_type Type_Void) pos
+    Nothing -> TError ["Unexpected continue statement at " ++ (show pos)]
+
+checkTypeReturnStatement :: Abs.STATEMENT Posn -> Env -> TCheckResult
+checkTypeReturnStatement node@(Abs.ReturnStatement pos ret) env = checkTypeReturnState ret env
+
+checkTypeReturnState :: Abs.RETURNSTATEMENT Posn -> Env -> TCheckResult
+checkTypeReturnState node@(Abs.ReturnState pos retExp) env = case Data.Map.lookup "return" env of
+    Just result -> checkTypeExpresion retExp env
+    Nothing -> TError ["Unexpected return at " ++ (show pos)]
+
+checkTypeExpresion :: Abs.EXPRESSION Posn -> Env -> TCheckResult
+checkTypeExpresion node@(Abs.ExpressionInteger pos value) env = checkTypeInteger value env
+checkTypeExpresion node@(Abs.ExpressionBoolean pos value) env = checkTypeBoolean value env
+checkTypeExpresion node@(Abs.ExpressionChar pos value) env = checkTypeChar value env
+checkTypeExpresion node@(Abs.ExpressionString pos value) env = checkTypeString value env
+checkTypeExpresion node@(Abs.ExpressionReal pos value) env = checkTypeReal value env
+
+checkTypeInteger :: Abs.Integer Posn -> Env -> TCheckResult
+checkTypeInteger node@(Abs.Integer value pos) env = TResult env (B_type Type_Integer ) pos
+
+checkTypeChar :: Abs.Char Posn -> Env -> TCheckResult
+checkTypeChar node@(Abs.Char value pos) env = TResult env (B_type Type_Char ) pos
+
+checkTypeString :: Abs.String Posn -> Env -> TCheckResult
+checkTypeString node@(Abs.String value pos) env = TResult env (B_type Type_String ) pos
+
+checkTypeReal :: Abs.Real Posn -> Env -> TCheckResult
+checkTypeReal node@(Abs.Real value pos) env = TResult env (B_type Type_Real ) pos
+
+checkTypeBoolean :: Abs.Boolean Posn -> Env -> TCheckResult
+checkTypeBoolean node@(Abs.Boolean_true pos) env = TResult env (B_type Type_Boolean ) pos
+checkTypeBoolean node@(Abs.Boolean_True pos) env = TResult env (B_type Type_Boolean ) pos
+checkTypeBoolean node@(Abs.Boolean_false pos) env = TResult env (B_type Type_Boolean ) pos
+checkTypeBoolean node@(Abs.Boolean_False pos) env = TResult env (B_type Type_Boolean ) pos
